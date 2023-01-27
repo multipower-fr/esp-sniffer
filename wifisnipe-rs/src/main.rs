e6be062eacac324fc39267cb1cb35b2e0b98d24b
Nicolas signed-log FORMICHELLA <stigpro@outlook.fr>
2023-01-27 17:29:39 +0100

    Testing 2

--- wifisnipe-rs/src/main.rs
+++ wifisnipe-rs/src/main.rs
@@ -1,8 +1,7 @@
 #[macro_use]
 extern crate lazy_static;
-extern crate actix;
 
-use std::collections::HashMap;
+use std::collections::{HashMap, VecDeque};
 use std::io::{self, Read};
 use std::str;
 use std::sync::Mutex;
@@ -10,6 +9,7 @@ use std::thread;
 use std::time::Duration;
 
 use actix::prelude::*;
+use ringbuf::HeapRb;
 use serialport;
 
 lazy_static! {
@@ -40,21 +40,17 @@ impl Actor for Parser {
 impl Handler<Frame> for Parser {
     type Result = ();
     fn handle(&mut self, msg: Frame, _ctx: &mut Self::Context) -> Self::Result {
-        let written = msg.1.clone();
-        let frame = (&(msg.0.clone())[0..written - 3]).to_vec();
-        println!("| {:?} |", &(msg.0)[0..written]);
-        if frame[0] == 2 && frame[written - 4] == 31 {
-            thread::spawn(move || {
-                parse(frame);
-            });
-        } else {
-            println!("{:?}", frame)
-        }
+        
     }
 }
 
 #[actix::main]
 async fn main() {
+    let mut rb = HeapRb::<Vec<u8>>::new(100);
+    let (mut tx, mut rx) = rb.split();
+    thread::spawn(move || {
+        parse(rx);
+    });
     // Lancer l'acteur
     let parser_addr = Parser.start();
     // Ouvrir le port serie
@@ -75,39 +71,41 @@ async fn main() {
     }
 }
 
-fn parse(frame: Vec<u8>) {
-    //
-    let channel: u8;
-    let mac_address: &str;
-    let rssi: &str;
-    let ssid: &str;
-    let str_frame: &str;
-    let splitted_frame: Vec<_>;
-    let mut frame_tmp = frame.clone();
-    
-    // Enleve le cractere de debut de transmission
-    frame_tmp.remove(0);
-    /* if frame_tmp[frame_tmp.len()-1] == 0x1F {
-        frame_tmp.remove(frame_tmp.len() -1);
-    }*/
-    // Convertis le vecteur de codes ASCII en chaine de caracteres
-    match str::from_utf8(&frame_tmp) {
-        Ok(f) => str_frame = f,
-        Err(_) => str_frame = "",
+fn parse() {
+    while 
+    loop {
+        let channel: u8;
+        let mac_address: &str;
+        let rssi: &str;
+        let ssid: &str;
+        let str_frame: &str;
+        let splitted_frame: Vec<_>;
+        let mut frame_tmp = frame.clone();
+
+        // Enleve le cractere de debut de transmission
+        frame_tmp.remove(0);
+        /* if frame_tmp[frame_tmp.len()-1] == 0x1F {
+            frame_tmp.remove(frame_tmp.len() -1);
+        }*/
+        // Convertis le vecteur de codes ASCII en chaine de caracteres
+        match str::from_utf8(&frame_tmp) {
+            Ok(f) => str_frame = f,
+            Err(_) => str_frame = "",
+        }
+        println!("{str_frame}");
+        // Split au niveau des caracteres de controle
+        splitted_frame = str_frame.split('\x1F').collect();
+        // println!("{:?}", splitted_frame);
+        // Check suivant si le channel est a un chiffre ou a deux chiffres
+        if frame_tmp[1] == 0x1F {
+            channel = frame_tmp[0] - 48;
+        } else {
+            channel = (splitted_frame[0]).parse::<u8>().unwrap()
+        }
+        // println!("{:}", channel);
+        mac_address = splitted_frame[1];
+        rssi = splitted_frame[2];
+        ssid = splitted_frame[splitted_frame.len() - 1];
+        // println!("{channel} {mac_address} {rssi} {ssid}");
     }
-    println!("{str_frame}");
-    // Split au niveau des caracteres de controle
-    splitted_frame = str_frame.split('\x1F').collect();
-    // println!("{:?}", splitted_frame);
-    // Check suivant si le channel est a un chiffre ou a deux chiffres
-    if frame_tmp[1] == 0x1F {
-        channel = frame_tmp[0] - 48;
-    } else {
-        channel = (splitted_frame[0]).parse::<u8>().unwrap()
-    }
-    // println!("{:}", channel);
-    mac_address = splitted_frame[1];
-    rssi = splitted_frame[2];
-    ssid = splitted_frame[splitted_frame.len() - 1];
-    // println!("{channel} {mac_address} {rssi} {ssid}");
 }

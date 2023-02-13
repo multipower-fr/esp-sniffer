8016de805211dcae239d8d2198d907dd311b470f
Nicolas signed-log FORMICHELLA <stigpro@outlook.fr>
2023-02-13 17:29:05 +0100

    Working + Timeout

--- wifisnipe-rs/src/main.rs
+++ wifisnipe-rs/src/main.rs
@@ -2,14 +2,15 @@
 extern crate lazy_static;
 extern crate futures;
 
-use itertools::sorted;
+use chrono::{DateTime, Local};
 use std::collections::HashMap;
-use std::env;
 use std::io;
 use std::mem::MaybeUninit;
 use std::str;
+use std::sync::atomic::{AtomicU64, Ordering};
 use std::sync::{Arc, Mutex};
 use std::thread;
+use std::time::SystemTime;
 
 use bytes::BytesMut;
 use futures::stream::StreamExt;
@@ -19,27 +20,41 @@ use tokio_serial::SerialPortBuilderExt;
 use tokio_util::codec::{Decoder, Encoder};
 
 lazy_static! {
-    static ref MACS: Mutex<Vec<String>> = {
+    // Listes des adresses MAC
+    static ref MACS: Arc<Mutex<Vec<String>>> = {
         let macs: Vec<String> = Vec::new();
-        Mutex::new(macs)
+        Arc::new(Mutex::new(macs))
     };
-    static ref CHANNELS: Mutex<HashMap<String, Vec<u8>>> = {
+    // HashMap avec les canaux ou certaines MAC sont visibles
+    static ref CHANNELS: Arc<Mutex<HashMap<String, Vec<u8>>>> = {
         let cha = HashMap::new();
-        Mutex::new(cha)
+        Arc::new(Mutex::new(cha))
     };
-    static ref SSIDS: Mutex<HashMap<String, Vec<String>>> = {
+    // HashMap avec les SSIDs récupérés
+    static ref SSIDS: Arc<Mutex<HashMap<String, Vec<String>>>> = {
         let s = HashMap::new();
-        Mutex::new(s)
+        Arc::new(Mutex::new(s))
     };
-    static ref RSSIS: Mutex<HashMap<String, Vec<String>>> = {
+    // HashMap avec le dernier RSSI
+    static ref RSSIS: Arc<Mutex<HashMap<String, i16>>> = {
         let r = HashMap::new();
-        Mutex::new(r)
+        Arc::new(Mutex::new(r))
     };
+    // HashMap avec les timestamp ou les addresses MAC sont vues en dernier
+    static ref LAST_SEEN: Arc<Mutex<HashMap<String, SystemTime>>> = {
+        let ts = HashMap::new();
+        Arc::new(Mutex::new(ts))
+    };
+    // Timestamp du dernier print (ici statique)
+    static ref LAST_PRINT: SystemTime = SystemTime::now();
+    // Nombre de print effectués
+    static ref PRINT_COUNT: AtomicU64 = AtomicU64::new(1);
 }
 
 #[cfg(windows)]
 const DEFAULT_TTY: &str = "COM3";
 
+// Structure utilisée pour scinder les informations reçues par lignes
 struct LineCodec;
 
 impl Decoder for LineCodec {
@@ -69,7 +84,8 @@ impl Encoder<String> for LineCodec {
 
 #[tokio::main]
 async fn main() -> tokio_serial::Result<()> {
-    let mut args = env::args();
+    // Récupère le port demandé ou fallback sur le défaut
+    let mut args = std::env::args();
     let tty_path = args.nth(1).unwrap_or_else(|| DEFAULT_TTY.into());
     // FIFO queue
     let data_queue = HeapRb::<String>::new(255);
@@ -79,11 +95,13 @@ async fn main() -> tokio_serial::Result<()> {
     thread::spawn(move || {
         parse_str(data_queue_rx);
     });
-    // Ouvrir le port serie
+    // Ouvre le port serie
     let port = tokio_serial::new(tty_path, 115_200).open_native_async()?;
+    // Déclare le lecteur
     let mut reader = LineCodec.framed(port);
     while let Some(line_result) = reader.next().await {
         let line = line_result.expect("Failed to read line");
+        // Si la ligne n'est pas mauvaise, la push sur le FIFO
         data_queue_tx.push(line).unwrap();
     }
     Ok(())
@@ -106,68 +124,130 @@ fn parse_str(mut data_queue_rx: Consumer<String, Arc<SharedRb<String, Vec<MaybeU
         let cleaned_frame: String = frame.replace(&['\u{2}', '\u{3}', '\r', '\n'][..], "");
         // Split au niveau des caracteres de controle
         let splitted_frame: Vec<_> = cleaned_frame.split('\x1F').collect();
-        if splitted_frame.len() != 4 || splitted_frame.len() != 5 {
-            println!("{:?}", splitted_frame);
-        }
         // Check suivant si le channel est a un chiffre ou a deux chiffres
         let channel: u8 = (splitted_frame[0]).parse::<u8>().unwrap_or(0);
         let mac_address: String = if MAC_REGEX.is_match(splitted_frame[1]) {
             splitted_frame[1].to_string()
         } else {
+            // Si l'addresse MAC est invalide
             "".to_string()
         };
+        // Récupère le RSSI
         let rssi: String = splitted_frame[2].to_string();
+        // Vérifie que le SSID n'est pas le RSSI
         let ssid: String = if splitted_frame[splitted_frame.len() - 2] != rssi {
             splitted_frame[splitted_frame.len() - 2].to_string()
         } else {
             splitted_frame[splitted_frame.len() - 1].to_string()
         };
-        println!("{channel} {mac_address} {rssi} {ssid}");
-        // TODO: Ajouter aux HashMaps
-        store(channel, mac_address, ssid)
+        // println!("{channel} {mac_address} {rssi} {ssid}");
+        store(channel, mac_address, rssi, ssid);
     }
 }
 
-fn store(channel: u8, mac_address: String, ssid: String) {
+fn store(channel: u8, mac_address: String, rssi: String, ssid: String) {
+    // Récupérer le lock sur les hashmaps
     let mut mac_table = MACS.lock().unwrap();
-    let mut channel_table = CHANNELS.lock().unwrap();
-
-    // let mut rssi_table = RSSIS.lock().unwrap();
+    // Ajouter a la liste des Adresses MAC connues si non dupliquées
     if !mac_table.contains(&mac_address) {
         mac_table.push(mac_address.clone())
     }
-    channel_table
-        .entry(mac_address.clone())
-        .and_modify(|channel_vec| {
-            if channel_vec.len() > 1 {
-                if channel_vec.contains(&channel) {
-                    channel_vec.remove(channel_vec.iter().rposition(|idx| *idx == channel).unwrap());
-                }
-            }
-        })
-        .or_insert_with(Vec::new)
-        .push(channel);
 
-    println!("CHANNEL: {:?}", channel_table.get(&*mac_address));
-    if ssid != "" {
-        let mut ssid_table = SSIDS.lock().unwrap();
-        ssid_table
-            .entry(mac_address.clone())
-            .and_modify(|ssid_vec| {
-                if ssid_vec.len() > 1 {
-                    if ssid_vec.contains(&ssid) {
-                        ssid_vec.remove(ssid_vec.iter().rposition(|idx| *idx == ssid).unwrap());
-                    }
-                }
-            })
-            .or_insert_with(Vec::new)
-            .push(ssid);
-        println!("SSIDs: {:?}", ssid_table.get(&*mac_address))
-    }
-    /*
-    rssi_table
+    // Mets a jour le timestamp
+    let mut timestamp_table = LAST_SEEN.lock().unwrap();
+    timestamp_table
         .entry(mac_address.clone())
-        .or_insert_with(Vec::new)
-        .push(rssi);
-    */
+        .and_modify(|ts_tmp| *ts_tmp = SystemTime::now())
+        .or_insert(SystemTime::now());
+
+    let mut channel_table = CHANNELS.lock().unwrap();
+    // S'assurer de la présence du vecteur dans la table
+    if let Some(tmp_channel) = channel_table.get(&mac_address) {
+        // Déduplication
+        if !tmp_channel.contains(&channel) {
+            channel_table
+                .entry(mac_address.clone())
+                .or_insert_with(Vec::new)
+                .push(channel);
+        }
+    } else {
+        // Le créer si pas présent
+        channel_table
+            .entry(mac_address.clone())
+            .or_insert_with(Vec::new)
+            .push(channel);
+    }
+    // println!("CHANNEL: {:?}", channel_table.get(&*mac_address));
+
+    // Ne rien faire si le SSID est vide
+    if !ssid.is_empty() {
+        // Récupérer le lock sur la table des SSIDs
+        let mut ssid_table = SSIDS.lock().unwrap();
+        // S'assurer de la présence du vecteur dans la table
+        if let Some(tmp_ssid) = ssid_table.get(&mac_address) {
+            // Déduplication
+            if !tmp_ssid.contains(&ssid) {
+                ssid_table
+                    .entry(mac_address.clone())
+                    .or_insert_with(Vec::new)
+                    .push(ssid);
+            }
+        }
+        // Le créer si pas présent
+        else {
+            ssid_table
+                .entry(mac_address.clone())
+                .or_insert_with(Vec::new)
+                .push(ssid);
+        }
+        // println!("SSIDs: {:?}", ssid_table.get(&*mac_address))
+    }
+    // RSSI
+    let mut rssi_table = RSSIS.lock().unwrap();
+    // Ajout du dernier RSSI vu
+    rssi_table
+        .entry(mac_address)
+        .and_modify(|rssi_tmp| *rssi_tmp = (rssi).parse::<i16>().unwrap_or(-50))
+        .or_insert((rssi).parse::<i16>().unwrap_or(-50));
+    // println!("RSSI {:?}", rssi_table.get(&mac_address));
+
+    // S'assurer que la clock n'a pas skew en arrière
+    if let Ok(dur) = LAST_PRINT.elapsed() {
+        // Augmenter la condition suivant le nombre de print pour contrecarrer la non mutabilité de SystemTime
+        if dur.as_secs() > PRINT_COUNT.load(Ordering::SeqCst) * 30 {
+            let seen_macs = Arc::clone(&MACS);
+            let seen_ssids = Arc::clone(&SSIDS);
+            let seen_channels = Arc::clone(&CHANNELS);
+            let seen_rssi = Arc::clone(&RSSIS);
+            let last_seen = Arc::clone(&LAST_SEEN);
+            thread::spawn(move || {
+                let seen_macs = seen_macs.lock().unwrap().clone();
+                let seen_ssids = seen_ssids.lock().unwrap().clone();
+                let seen_channels = seen_channels.lock().unwrap().clone();
+                let seen_rssi = seen_rssi.lock().unwrap().clone();
+                let last_seen = last_seen.lock().unwrap().clone();
+                println!(
+                    "---------- {} ----------",
+                    Local::now().format("%Y-%m-%d][%H:%M:%S")
+                );
+                for mac in seen_macs.into_iter() {
+                    // Convertis le timestamp dans un format qui permets l'affichage
+                    let seen_ts: DateTime<Local> = (*last_seen.get(&mac.clone()).unwrap()).into();
+                    let diff = Local::now() - seen_ts;
+                    if diff.num_seconds() >= 60 {
+                        continue;
+                    }
+                    println!(
+                        "{mac} | Last seen : {} | {:?} | {:?} | {:?}",
+                        seen_ts.format("%Y-%m-%d -- %H:%M:%S"),
+                        seen_channels.get(&mac.clone()).unwrap(),
+                        seen_ssids.get(&mac.clone()),
+                        seen_rssi.get(&mac.clone()).unwrap()
+                    )
+                }
+            });
+            // Ajoute un au PRINT_COUNT
+            PRINT_COUNT.fetch_add(1, Ordering::SeqCst);
+        }
+    }
 }

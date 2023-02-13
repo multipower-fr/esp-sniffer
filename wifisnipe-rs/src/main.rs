386d7ccd7a1945f5ac84cda1038d95b6a3a64a46
Nicolas signed-log FORMICHELLA <stigpro@outlook.fr>
2023-02-13 09:26:42 +0100

    Clean code

--- wifisnipe-rs/src/main.rs
+++ wifisnipe-rs/src/main.rs
@@ -10,19 +10,18 @@ use std::thread;
 use std::time::Duration;
 
 use ringbuf::{Consumer, HeapRb, SharedRb};
-use serialport;
 
 lazy_static! {
     static ref CHANNELS: Mutex<HashMap<String, Vec<u8>>> = {
-        let mut cha = HashMap::new();
+        let cha = HashMap::new();
         Mutex::new(cha)
     };
     static ref SSIDS: Mutex<HashMap<String, Vec<String>>> = {
-        let mut s = HashMap::new();
+        let s = HashMap::new();
         Mutex::new(s)
     };
     static ref RSSIS: Mutex<HashMap<String, Vec<String>>> = {
-        let mut r = HashMap::new();
+        let r = HashMap::new();
         Mutex::new(r)
     };
 }
@@ -45,7 +44,7 @@ fn main() {
     let mut serial_buf: Vec<u8> = vec![0; 1000];
     // Cache la valeur de retour du send
     let mut _res;
-    // TODO: Revoir ce qu'est 
+    // TODO: Revoir ce qu'est
     loop {
         match port.read(serial_buf.as_mut_slice()) {
             Ok(_) => _res = tx.push(serial_buf.to_vec()),
@@ -62,12 +61,6 @@ fn parse(mut queue_rx: Consumer<Vec<u8>, Arc<SharedRb<Vec<u8>, Vec<MaybeUninit<V
         }
         // Recupere un element de la FIFO
         let frame: Vec<u8> = queue_rx.pop().unwrap();
-        let channel: u8;
-        let mac_address: &str;
-        let rssi: &str;
-        let ssid: &str;
-        let str_frame: &str;
-        let splitted_frame: Vec<_>;
         let mut frame_tmp: Vec<u8> = frame.clone();
         // TODO: Faire le découpage
         // Enleve le cractere de debut de transmission
@@ -76,26 +69,26 @@ fn parse(mut queue_rx: Consumer<Vec<u8>, Arc<SharedRb<Vec<u8>, Vec<MaybeUninit<V
             frame_tmp.remove(frame_tmp.len() -1);
         }*/
         // Convertis le vecteur de codes ASCII en chaine de caracteres
-        match str::from_utf8(&frame_tmp) {
-            Ok(f) => str_frame = f,
-            Err(_) => str_frame = "ERROR",
-        }
+        let str_frame: &str = match str::from_utf8(&frame_tmp) {
+            Ok(f) => f,
+            Err(_) => "ERROR",
+        };
         println!("{str_frame}");
 
         // Split au niveau des caracteres de controle
-        splitted_frame = str_frame.split('\x1F').collect();
+        let splitted_frame: Vec<_> = str_frame.split('\x1F').collect();
         // println!("{:?}", splitted_frame);
         // Check suivant si le channel est a un chiffre ou a deux chiffres
-        if frame_tmp[1] == 0x1F {
+        let channel: u8 = if frame_tmp[1] == 0x1F {
             // Enlever 48 pour retourner du code ASCII au numéro
-            channel = frame_tmp[0] - 48;
+            frame_tmp[0] - 48
         } else {
-            channel = (splitted_frame[0]).parse::<u8>().unwrap()
-        }
+            (splitted_frame[0]).parse::<u8>().unwrap()
+        };
         // println!("{:}", channel);
-        mac_address = splitted_frame[1];
-        rssi = splitted_frame[2];
-        ssid = splitted_frame[splitted_frame.len() - 1];
+        let mac_address: &str = splitted_frame[1];
+        let rssi: &str = splitted_frame[2];
+        let ssid: &str = splitted_frame[splitted_frame.len() - 1];
         println!("{channel} {mac_address} {rssi} {ssid}");
         // TODO: Ajouter aux HashMaps
     }
